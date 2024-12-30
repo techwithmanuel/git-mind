0059dd87c26568f211a1e907e5d8ed6a53b06d0a
Emmanuel Alozie <emmanuelalozie69@gmail.com>
2024-12-30 23:35:12 +0100

    feat: Init git commit and add support for multiple file changes  - Add functionality to initialize a git commit from multiple file changes. - Improve error handling and messaging during the git commit process. - Refactor code for maintainability and readability.

--- src/index.ts
+++ src/index.ts
@@ -1,131 +1,39 @@
 #!/usr/bin/env node
 process.noDeprecation = true;
 
-import { gitDiffForFile } from "./git/changes/diff.js";
-import { checkGitStatus } from "./git/changes/files.js";
-import { createGitCommit, hasPreferredModel } from "./git/commit.js";
-import { verifyRemoteRepo, pushToRemoteRepo } from "./git/remote.js";
-import { getClaudeAPIKey } from "./models/claude/get-key.js";
-import { registerClaudeAPIKey } from "./models/claude/manage.js";
-import { getGeminiAPIKey } from "./models/gemini/get-key.js";
-import { registerGeminiAPIKey } from "./models/gemini/manage.js";
-import { getGPTAPIKey } from "./models/gpt/get-key.js";
-import { registerGPTAPIKey } from "./models/gpt/manage.js";
-import { Model, selectModel } from "./models/select.js";
-import { intro, outro, log, note } from "@clack/prompts";
-import chalk from "chalk";
-import { terminalCommand } from "./utils/command/index.js";
-import { awaitingFnCall } from "./utils/sleep/index.js";
+import { Command } from "commander";
+import { initGitCommit } from "./git/init.js";
+import { manageGeminiAPIKey } from "./models/gemini/manage.js";
+import { manageClaudeAPIKey } from "./models/claude/manage.js";
+import { manageGPTAPIKey } from "./models/gpt/manage.js";
 
-async function validateAPIKey(model: Model): Promise<boolean> {
-  let key;
+const program = new Command();
 
-  if (model === "claude") {
-    key = getClaudeAPIKey();
-  } else if (model === "gpt") {
-    key = getGPTAPIKey();
-  } else if (model === "gemini") {
-    key = getGeminiAPIKey();
-  }
+program
+  .name("git-mind")
+  .description(
+    "A CLI tool that generates intelligent git commit messages using AI, with support for Gemini, ChatGPT, and Claude AI."
+  )
+  .version("0.0.1");
 
-  if (!key || key.length === 0) {
-    return false;
-  }
+program
+  .command("init")
+  .description("Initialize Git Commit")
+  .action(initGitCommit);
 
-  return true;
-}
+program
+  .command("manage-gemini")
+  .description("Manage the Gemini model")
+  .action(manageGeminiAPIKey);
 
-async function registerKey(model: Model) {
-  const registrations = {
-    claude: registerClaudeAPIKey,
-    gpt: registerGPTAPIKey,
-    gemini: registerGeminiAPIKey,
-  };
-  await registrations[model]();
-}
+program
+  .command("manage-claude")
+  .description("Manage the Claude model")
+  .action(manageClaudeAPIKey);
 
-async function processGitChanges(files: string[]): Promise<void> {
-  const file_names = files.join(", ");
+program
+  .command("manage-gpt")
+  .description("Manage the GPT model")
+  .action(manageGPTAPIKey);
 
-  log.info(chalk.gray(`Changed Files: ${file_names}`));
-
-  if (files.length > 5) {
-    log.info("More than 5 files, running git add .");
-    terminalCommand("git add .");
-
-    const diffs = files.slice(0, 2).map((file: string) => {
-      const diff = gitDiffForFile(file);
-      return diff ? diff : "";
-    });
-
-    const combinedDiff = diffs.join("\n");
-
-    const commitMessage = await awaitingFnCall<string | undefined>(
-      () => createGitCommit(combinedDiff),
-      `Generated commit messages for ${file_names}`
-    );
-
-    if (commitMessage) {
-      note(commitMessage);
-
-      const formattedCommitMessage = commitMessage.replace(/\n/g, " ");
-
-      terminalCommand(`git commit -m  "${formattedCommitMessage}"`);
-    }
-  } else {
-    for (const file of files) {
-      log.info(`Staging file: ${file}`);
-
-      terminalCommand(`git add ${file}`);
-
-      const diff = gitDiffForFile(file);
-
-      if (diff) {
-        const commitMessage = await awaitingFnCall<string | undefined>(
-          () => createGitCommit(diff),
-          `Generated commit message from ${file}`
-        );
-
-        if (commitMessage) {
-          note(commitMessage);
-
-          const formattedCommitMessage = commitMessage.replace(/\n/g, " ");
-
-          terminalCommand(`git commit -m  "${formattedCommitMessage}"`);
-        }
-      }
-    }
-  }
-
-  await pushToRemoteRepo();
-}
-
-async function initGitCommit() {
-  intro(chalk.gray("Git Mind Initialized 🔎 "));
-
-  try {
-    await verifyRemoteRepo();
-    const files = checkGitStatus();
-
-    let model = await hasPreferredModel();
-
-    if (!model) {
-      await selectModel();
-    } else {
-      const hasValidKey = await validateAPIKey(model);
-      if (!hasValidKey) {
-        await registerKey(model);
-        return;
-      } else {
-        await processGitChanges(files);
-      }
-    }
-  } catch (error) {
-    console.error("Error:", error);
-    process.exit(1);
-  }
-
-  outro(chalk.blue("All commits resolved 🎉"));
-}
-
-initGitCommit();
+program.parse(process.argv);
